01bbc54892c34a3f500ff0f8747a9bb1b5423bcf
Chandhana2004“ <chandhanabanda29@gmail.com>
2025-11-16 13:59:44 +0530

    Initial commit

---
+++ obj_detection_yolo.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret , frame = cap.read()
+    results = model(frame)
+    cv2.imshow("Object Detection Using YOLO",results[0].plot())
+
+    if cv2.waitKey(1)==27:
+        break
+
+cap.release
+cv2.destroyAllWindows
